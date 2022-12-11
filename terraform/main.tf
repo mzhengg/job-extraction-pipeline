c17682fece9e386dfd184004868ad90bcbefd64c
Michael Zheng <99830293+mzhengg@users.noreply.github.com>
2022-12-11 16:30:40 -0500

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "data-lake-bucket" {
+  bucket = "mzheng-${var.aws_s3_bucket}"
+
+  tags = {
+    Name = "My bucket"
+    Environment = "Dev"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_s3_bucket" {
+  description = "Name of bucket where data will be stored."
+  type = string
+  default = "indeed-job-posts"
+}
